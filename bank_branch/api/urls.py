5e025bcf136add98b62d137633eda290c5a51276
PhilippNikitin <54664800+PhilippNikitin@users.noreply.github.com>
2023-10-12 17:41:24 +0300

    Update urls.py
    add path for finding best banks

--- bank_branch/api/urls.py
+++ bank_branch/api/urls.py
@@ -7,5 +7,6 @@ app_name = 'api'
 urlpatterns = [
     path('map-banks/', BanksWithinMapBoundsView.as_view(), name='map_bank'),
     path('bank/', BankDetailsView.as_view(), name='bank_detail'),
+    path('best-banks/', BestBankView.as_view(), name='best-banks'),
 
 ]
